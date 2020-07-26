36cd5805b9128706c591c80468f84cb97be683ac
Praveen Shettigar <praveenshettigar@Praveens-MacBook-Air.local>
2020-07-26 17:43:42 -0500

    Moving to github

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "azurerm" {
   features { }  
 }
 resource "azurerm_resource_group" "rg" {
-  name     = var.resourceGroupName
+  name     = var.resource_group_name
   location = var.location
 }
